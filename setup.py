064b2ebd8ecfcb08671802278e79baabf7be1f52
Tobias Raabe <raabe@posteo.de>
2020-07-22 19:19:42 +0200

    Release v0.0.2. (#2)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pytask-latex",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={"pytask": ["pytask_latex = pytask_latex.plugin"]},
