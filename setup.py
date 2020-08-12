377c4f34f836bd1b88527f3b795a501fde8d7faa
Tobias Raabe <raabe@posteo.de>
2020-08-12 15:25:43 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pytask-latex",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={"pytask": ["pytask_latex = pytask_latex.plugin"]},
