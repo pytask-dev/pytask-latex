925d5f9079fdf4db52b9601f0e215124bddc1c53
Tobias Raabe <raabe@posteo.de>
2020-09-12 10:19:32 +0200

    Fix decorator.

--- src/pytask_latex/config.py
+++ src/pytask_latex/config.py
@@ -5,4 +5,4 @@ from _pytask.config import hookimpl
 @hookimpl
 def pytask_parse_config(config):
     """Register the latex marker in the configuration."""
-    config["markers"]["latex"] = "latex: Tasks which compile LaTeX documents."
+    config["markers"]["latex"] = "Tasks which compile LaTeX documents."
