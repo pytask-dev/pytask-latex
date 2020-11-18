f2305d7c757901f23eb531cefc1e4014cad74c06
Tobias Raabe <raabe@posteo.de>
2020-11-18 23:27:43 +0100

    adjusts test for v0.0.10.

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -102,10 +102,10 @@ def test_pytask_collect_task_teardown(
 
     task = DummyClass()
     task.depends_on = {
-        i: FilePathNode(n.split(".")[0], Path(n)) for i, n in enumerate(depends_on)
+        i: FilePathNode.from_path(Path(n)) for i, n in enumerate(depends_on)
     }
     task.produces = {
-        i: FilePathNode(n.split(".")[0], Path(n)) for i, n in enumerate(produces)
+        i: FilePathNode.from_path(Path(n)) for i, n in enumerate(produces)
     }
     task.markers = [Mark("latex", (), {})]
     task.function = task_dummy
