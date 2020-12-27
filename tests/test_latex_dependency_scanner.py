a7ad3952ac60550258e3df7a955be4b5723ee555
Tobias Raabe <raabe@posteo.de>
2020-12-27 22:52:13 +0100

    Add test to check scanning of latex files.

---
+++ tests/test_latex_dependency_scanner.py
@@ -0,0 +1,34 @@
+import textwrap
+
+import pytest
+from pytask import main
+
+
+@pytest.mark.end_to_end
+def test_infer_dependencies_from_task(tmp_path):
+    task_source = """
+    import pytask
+
+    @pytask.mark.latex
+    @pytask.mark.depends_on("document.tex")
+    @pytask.mark.produces("document.pdf")
+    def task_compile_document():
+        pass
+
+    """
+    tmp_path.joinpath("task_dummy.py").write_text(textwrap.dedent(task_source))
+
+    latex_source = r"""
+    \documentclass{report}
+    \begin{document}
+    \input{sub_document}
+    \end{document}
+    """
+    tmp_path.joinpath("document.tex").write_text(textwrap.dedent(latex_source))
+    tmp_path.joinpath("sub_document.tex").write_text("Lorem ipsum.")
+
+    session = main({"paths": tmp_path})
+
+    assert session.exit_code == 0
+    assert len(session.tasks) == 1
+    assert len(session.tasks[0].depends_on) == 2
