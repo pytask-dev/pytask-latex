e83f3c7f729d0c325c986abaedfa20ddf798cac7
Tobias Raabe <raabe@posteo.de>
2021-02-25 23:04:59 +0100

    Fix long description and coverage.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
 import versioneer
 
 
+README = Path("README.rst").read_text()
+
 PROJECT_URLS = {
     "Documentation": "https://github.com/pytask-dev/pytask-latex",
     "Github": "https://github.com/pytask-dev/pytask-latex",
     "Tracker": "https://github.com/pytask-dev/pytask-latex/issues",
-    "Changelog": "https://github.com/pytask-dev/pytask-latex/blob/main/" "CHANGES.rst",
+    "Changelog": "https://github.com/pytask-dev/pytask-latex/blob/main/CHANGES.rst",
 }
 
 
@@ -17,6 +21,8 @@ setup(
     version=versioneer.get_version(),
     cmd_class=versioneer.get_cmdclass(),
     description="Compile LaTeX documents with pytask.",
+    long_description=README,
+    long_description_content_type="text/x-rst",
     author="Tobias Raabe",
     author_email="raabe@posteo.de",
     python_requires=">=3.6",
