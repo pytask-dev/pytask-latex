602f7fc7930bbebb8b9c3987340c74640468424a
Tobias Raabe <raabe@posteo.de>
2021-06-23 21:38:23 +0200

    Replace versioneer with setuptools-scm. (#24)

--- setup.py
+++ setup.py
@@ -1,53 +1,5 @@
-from pathlib import Path
-
-from setuptools import find_packages
 from setuptools import setup
 
-import versioneer
 
-
-README = Path("README.rst").read_text()
-
-PROJECT_URLS = {
-    "Documentation": "https://github.com/pytask-dev/pytask-latex",
-    "Github": "https://github.com/pytask-dev/pytask-latex",
-    "Tracker": "https://github.com/pytask-dev/pytask-latex/issues",
-    "Changelog": "https://github.com/pytask-dev/pytask-latex/blob/main/CHANGES.rst",
-}
-
-
-setup(
-    name="pytask-latex",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Compile LaTeX documents with pytask.",
-    long_description=README,
-    long_description_content_type="text/x-rst",
-    author="Tobias Raabe",
-    author_email="raabe@posteo.de",
-    url=PROJECT_URLS["Github"],
-    project_urls=PROJECT_URLS,
-    license="MIT",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    install_requires=[
-        "click",
-        "latex-dependency-scanner",
-        "pytask >= 0.0.11",
-    ],
-    python_requires=">=3.6",
-    entry_points={"pytask": ["pytask_latex = pytask_latex.plugin"]},
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    platforms="any",
-    include_package_data=True,
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
--- src/pytask_latex/__init__.py
+++ src/pytask_latex/__init__.py
@@ -1,4 +1,9 @@
-from ._version import get_versions
+try:
+    from ._version import version as __version__
+except ImportError:
+    # broken installation, we don't even try unknown only works because we do poor mans
+    # version compare
+    __version__ = "unknown"
 
-__version__ = get_versions()["version"]
-del get_versions
+
+__all__ = ["__version__"]
