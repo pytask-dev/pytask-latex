0ae5cd738eb7788f5b5bc346142a4ee8b2be7ae4
Tobias Raabe <raabe@posteo.de>
2023-01-09 18:05:21 +0100

    Fix pre-commit.

--- src/pytask_latex/config.py
+++ src/pytask_latex/config.py
@@ -2,13 +2,12 @@
 from __future__ import annotations
 
 from typing import Any
-from typing import Callable
 
 from pytask import hookimpl
 
 
 @hookimpl
-def pytask_parse_config(config):
+def pytask_parse_config(config: dict[str, Any]) -> None:
     """Register the latex marker in the configuration."""
     config["markers"]["latex"] = "Tasks which compile LaTeX documents."
     config["infer_latex_dependencies"] = config.get("infer_latex_dependencies", True)
