ba41fbfd88255be3afe051bdadd440349b5a87be
Tobias Raabe <raabe@posteo.de>
2023-10-04 13:00:55 +0200

    fix.

--- src/pytask_latex/execute.py
+++ src/pytask_latex/execute.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 
 import shutil
 
-from pytask import has_mark
+from pytask import PTask, has_mark
 from pytask import hookimpl
 from pytask import Task
 
 
 @hookimpl
-def pytask_execute_task_setup(task: Task) -> None:
+def pytask_execute_task_setup(task: PTask) -> None:
     """Check that latexmk is found on the PATH if a LaTeX task should be executed."""
     if has_mark(task, "latex") and shutil.which("latexmk") is None:
         raise RuntimeError(
