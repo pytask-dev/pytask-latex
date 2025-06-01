95e8c9847f00fefce4884a724bf55bc56103521f
Tobias Raabe <raabe@posteo.de>
2025-06-01 19:25:43 +0200

    Defer latexmk check after skips.

--- src/pytask_latex/execute.py
+++ src/pytask_latex/execute.py
@@ -9,9 +9,10 @@ from pytask import has_mark
 from pytask import hookimpl
 
 
-@hookimpl
+@hookimpl(trylast=True)
 def pytask_execute_task_setup(task: PTask) -> None:
     """Check that latexmk is found on the PATH if a LaTeX task should be executed."""
+    _rich_traceback_omit = True
     if has_mark(task, "latex") and shutil.which("latexmk") is None:
         msg = (
             "latexmk is needed to compile LaTeX documents, but it is not found on "
