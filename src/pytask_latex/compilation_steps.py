306749b6a4c696709fcc3858cae6f8d1aa86a761
Tobias Raabe <raabe@posteo.de>
2025-06-01 17:21:09 +0200

    Fix issue.

--- src/pytask_latex/compilation_steps.py
+++ src/pytask_latex/compilation_steps.py
@@ -18,7 +18,6 @@ from typing import TYPE_CHECKING
 from typing import Any
 from typing import Callable
 
-from pytask_latex.path import relative_to
 from pytask_latex.utils import to_list
 
 if TYPE_CHECKING:
@@ -38,9 +37,7 @@ def latexmk(
 
     def run_latexmk(path_to_tex: Path, path_to_document: Path) -> None:
         job_name_opt = [f"--jobname={path_to_document.stem}"]
-        out_dir_opt = [
-            f"--output-directory={relative_to(path_to_tex, path_to_document.parent)}"
-        ]
+        out_dir_opt = [f"--output-directory={path_to_document.parent.as_posix()}"]
         cmd = ["latexmk", *options, *job_name_opt, *out_dir_opt, path_to_tex.as_posix()]
         subprocess.run(cmd, check=True)  # noqa: S603
 
