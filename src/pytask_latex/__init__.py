4f66ee3b548c108ca6eab2cdc80f4d4788b907d1
Tobias Raabe <raabe@posteo.de>
2025-12-30 15:31:01 +0100

    Fix ty ignores

--- src/pytask_latex/__init__.py
+++ src/pytask_latex/__init__.py
@@ -3,9 +3,7 @@
 from __future__ import annotations
 
 try:
-    from ._version import (
-        version as __version__,  # ty: ignore[unresolved-import, unused-ignore-comment]
-    )
+    from ._version import version as __version__  # ty: ignore[unresolved-import]
 except ImportError:  # pragma: no cover
     # broken installation, we don't even try unknown only works because we do poor mans
     # version compare
